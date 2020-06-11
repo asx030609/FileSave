4e58c3bb5e621455a984f9fd6d1429ccfebe24cb
asx030609 <479164736@qq.com>
2020-06-11 21:17:47 +0800

    优化 存储过程模板，新增出现异常时，回退

--- LYYC_FLK，oracleSql/终点0-testOracle函数存储过程等等/试试存储过程.sql
+++ LYYC_FLK，oracleSql/终点0-testOracle函数存储过程等等/试试存储过程.sql
@@ -1,16 +1,30 @@
 
 create or replace procedure pro_test
-(sex VARCHAR2)
+(sex IN VARCHAR2, V_RESULT OUT VARCHAR2)
 AS
   countNum number(10);
 BEGIN
   countNum := 1;
   dbms_output.put_line(sex);
+  
+  UPDATE WM_REGIONU SET ACTIVEU=0;
+  --SELECT ACTIVEU INTO countNum FROM BI_BRANDU;
+  
+  Exception when others then
+    raise;
+  V_RESULT := '^_^';
 END;
 /
 set serveroutput on;
+DECLARE 
+  V_RESULT VARCHAR2(50);
 BEGIN
-  pro_test('2323');
+  pro_test('2323', V_RESULT);
+  dbms_output.put_line(V_RESULT);
+  commit;
+  Exception when others then
+    rollback;
+    raise;
 END;
 
 
