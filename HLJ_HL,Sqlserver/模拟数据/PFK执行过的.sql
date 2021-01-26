b0e09e491c72e01e323e107385e64012940a2ab1
asx030609 <479164736@qq.com>
2021-01-26 09:14:00 +0800

    调整

---
+++ HLJ_HL,Sqlserver/模拟数据/PFK执行过的.sql
@@ -0,0 +1,9 @@
+
+
+
+SELECT * FROM WM_OutBillAllot;
+
+
+SELECT * FROM WM_OutBill WHERE OutBillNo='O202101220004'; --此出库单为 空托盘出库
+SELECT * FROM WM_OutBill WHERE OutBillNo='O202101220002'; --此出库单为 调剂出库
+SELECT * FROM WM_OutBillDetail;
