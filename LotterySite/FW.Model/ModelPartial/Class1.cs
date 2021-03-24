c6694ff30787a47eb2971bbe0998be4c8d731924
asx030609 <479164736@qq.com>
2021-03-24 15:21:45 +0800

    调整

---
+++ LotterySite/FW.Model/ModelPartial/Class1.cs
@@ -0,0 +1,13 @@
+﻿using BaseClassUtils;
+namespace FW.Common.T4Ttinclude
+{
+	class Class1
+	{
+		public static void Fun()
+		{
+			string ConnectionString = "";
+			SQLiteHelper.ConnectionString = ConnectionString;
+			var tbData = SQLiteHelper.ExecuteDataSet("SELECT * FROM ReplaceString");
+		}
+	}
+}
