a9fddf877fbbdb38edddeec8c2f5ca59026dd9db
asx030609 <479164736@qq.com>
2021-06-19 13:12:04 +0800

    调整 循环ping，这些命令最终结果会包含反馈192.168.1.1 的 Ping 统计信息.

--- 使用bat操作IP/bat示例内置HN.LY/循环ping.bat
+++ 使用bat操作IP/bat示例内置HN.LY/循环ping.bat
@@ -3,16 +3,8 @@
 d:
 
 for /L %%i in (1,1,%999999999%) do (
-echo %1  >>ping.txt
-echo 1% >>ping.txt
-echo %% >>ping.txt
+ping %1192.168.1.1 -n 1% && echo %% >>ping.txt
 echo %date%%time% >> ping.txt
 )
-@echo on
-echo %date%%time% >> ping.txt
-@echo ready show
-echo %%
-echo 1%
-@echo %1&&dir
 
 pause
